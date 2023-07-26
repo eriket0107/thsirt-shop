92165fd20b8bcb7c8f18ca1873d7d91af3814231
eriket0107 <ol.erik0107@gmail.com>
2023-07-26 14:43:33 -0300

    adjusting home class

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Slider } from '@/components/Slider'
 
 export default function Home() {
   return (
-    <main className="ml-auto flex min-h-home w-full max-w-spaceLeft gap-12">
+    <main className="min-h-home ml-auto flex w-full max-w-spaceLeft gap-12">
       <Slider>
         <Product />
         <Product />
