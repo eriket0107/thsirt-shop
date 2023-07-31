2dde1f5e4def996a596d7a6361ef46316aeb2c6b
eriket0107 <ol.erik0107@gmail.com>
2023-07-31 11:41:47 -0300

    adding libs

--- src/components/Slider/components/Arrow.tsx
+++ src/components/Slider/components/Arrow.tsx
@@ -6,25 +6,25 @@ export function Arrow({ isLeft, onClick }: ArrowType) {
   return (
     <div
       className={`
-      z-1 
-      absolute 
-      flex 
-      h-full
-      w-32 
-      flex-1 
-      cursor-pointer 
-      items-center 
-      justify-center 
-      bg-gradient-to-r 
-      from-transparent 
-      to-gray-900
-      shadow-md
-      ${isLeft ? 'scale-x-[-1]' : 'scale-x-1'}
-      ${isLeft ? 'left-0' : 'right-0'}
-      opacity-0
-      transition-transform
-      hover:opacity-100
-      hover:transition-all
+        z-1 
+        absolute 
+        flex 
+        h-full
+        w-32 
+        flex-1 
+        cursor-pointer 
+        items-center 
+        justify-center 
+        bg-gradient-to-r 
+        from-transparent 
+        to-gray-900
+        shadow-md
+        ${isLeft ? 'scale-x-[-1]' : 'scale-x-1'}
+        ${isLeft ? 'left-0' : 'right-0'}
+        opacity-0
+        transition-transform
+        hover:opacity-100
+        hover:transition-all
       `}
       onClick={onClick}
     >
