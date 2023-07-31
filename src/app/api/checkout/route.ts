dc2b6d5d0e414fd807d6a941902902c741c6ede1
eriket0107 <ol.erik0107@gmail.com>
2023-07-31 11:34:03 -0300

    feat: creating api route

---
+++ src/app/api/checkout/route.ts
@@ -0,0 +1,34 @@
+import { stripe } from '@/lib/stripe'
+import { NextResponse } from 'next/server'
+
+interface DataRequest {
+  priceId: string
+}
+
+export async function GET() {
+  return NextResponse.json({ Message: 'Hello' })
+}
+
+export async function POST(req: Request) {
+  const { priceId }: DataRequest = await req.json()
+
+  if (!priceId)
+    return NextResponse.json({ Error: 'Price not found', status: 400 })
+
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
+  const cancelUrl = `${process.env.NEXT_URL}/`
+
+  const checkoutUrl = await stripe.checkout.sessions.create({
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+    mode: 'payment',
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
+  })
+
+  return NextResponse.json({ checkoutUrl: checkoutUrl.url, status: 201 })
+}
