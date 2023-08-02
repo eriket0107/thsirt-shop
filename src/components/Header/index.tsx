e3eebc5f358c4cc8e6c2b2c6c7461c64d163c029
eriket0107 <ol.erik0107@gmail.com>
2023-08-02 20:17:02 -0300

    feat: adapting success page to handle multiple items

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Link from 'next/link'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 
 import logo from '@/assets/logo.svg'
 
@@ -12,28 +13,39 @@ import { useCart } from '@/contexts/cartContext'
 
 export function Header() {
   const { cart, onOpenCart } = useCart()
+  const path = usePathname()
 
   const cartLength = cart.length > 0 ? cart.length : 0
 
   return (
-    <header className="mx-auto flex w-full max-w-6xl items-center justify-between py-8">
+    <header
+      className="
+        mx-auto 
+        flex 
+        w-full 
+        max-w-6xl 
+        items-center 
+        justify-between 
+        py-8"
+    >
       <Link href={'/'}>
         <Image src={logo} alt="Logo Ignite" />
       </Link>
-
-      <ButtonIcon
-        className="
+      {path === 'success' && (
+        <ButtonIcon
+          cartLength={cartLength}
+          icon={<ShoppingBag size={24} />}
+          onClick={onOpenCart}
+          className="
           relative 
           cursor-pointer 
           rounded-md 
-          bg-gray-80 
-          p-3 
-          transition-all 
-          hover:opacity-75"
-        cartLength={cartLength}
-        icon={<ShoppingBag size={24} />}
-        onClick={onOpenCart}
-      />
+            bg-gray-80 
+            p-3 
+            transition-all 
+            hover:opacity-75"
+        />
+      )}
     </header>
   )
 }
